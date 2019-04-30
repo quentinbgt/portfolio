fc0b719c857f2e6e01dd13260ca8142a3393264c
quentinbgt <50140767+quentinbgt@users.noreply.github.com>
2019-04-30 14:01:16 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+include 'Vues/v_nav.php';
+
+include 'Vues/v_accueil.php';
+
+include 'Vues/v_competences.php';
+
+include 'Vues/v_portfolio.php';
+
+include 'Vues/v_stage.php';
+
+include 'Vues/v_patrimoine.php';
+
+include 'Vues/v_veille.php';
+
+include 'Vues/v_footer.php';
+?>
